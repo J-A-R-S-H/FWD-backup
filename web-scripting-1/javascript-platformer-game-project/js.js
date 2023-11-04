5ce44df66effbb4e58f78c9e9dc8644c24a9a2f9
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2023-11-04 16:39:59 -0700

    refactored code

--- web-scripting-1/javascript-platformer-game-project/js.js
+++ web-scripting-1/javascript-platformer-game-project/js.js
@@ -7,26 +7,44 @@ canvas.height = 64 * 9 //576
 
 
 
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+    }
 
-let y = 100
-const height = 100
-let bottom = y + 100
+    draw() {
+        c.fillStyle = "red"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 
+    update() {
+        if (this.sides.bottom < canvas.height) {
+            this.position.y++
+            this.sides.bottom = this.position.y + this.height
+        }
+    }
+}
+
+
+
+const player = new Player()
 
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = "white"
     c.fillRect(0, 0, canvas.width, canvas.height)
 
-    c.fillStyle = "red"
-    c.fillRect(100, y, 100, height)
 
-    if (bottom < canvas.height) {
-        y++
-        bottom = y + 100
-    }
-
-
+    player.draw()
+    player.update()
 }
 
 animate()
