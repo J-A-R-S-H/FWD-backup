6837a3f373d727f96687dff55f75dbc7cf7debbd
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2023-11-04 19:10:00 -0700

    finished player movement

---
+++ web-scripting-1/javascript-platformer-game-project/scripts/eventlisteners.js
@@ -0,0 +1,34 @@
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "w":
+            if (player.velocity.y === 0) {
+                player.velocity.y = -20
+            }
+            break
+        case "a":
+            keys.a.pressed = true
+            player.velocity.x = -4
+            break
+
+        case "d":
+            keys.d.pressed = true
+            player.velocity.x = 4
+            break
+    }
+})
+
+window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+
+        case "a":
+            keys.a.pressed = false
+            player.velocity.x = 0
+            break
+
+        case "d":
+            keys.d.pressed = false
+            player.velocity.x = 0
+            break
+    }
+})
--- web-scripting-1/javascript-platformer-game-project/scripts/js.js
+++ web-scripting-1/javascript-platformer-game-project/scripts/js.js
@@ -6,7 +6,19 @@ canvas.width = 64 * 16 //1024
 canvas.height = 64 * 9 //576 
 
 
+const keys = {
+    w: {
+        pressed: false
+    },
 
+    a: {
+        pressed: false
+    },
+
+    d: {
+        pressed: false
+    },
+}
 
 
 function animate() {
@@ -14,39 +26,16 @@ function animate() {
     c.fillStyle = "white"
     c.fillRect(0, 0, canvas.width, canvas.height)
 
+    player.velocity.x = 0
+    if (keys.d.pressed) {
+        player.velocity.x = 4
+    }
+    else if (keys.a.pressed) {
+        player.velocity.x = -4
+    }
 
     player.draw()
     player.update()
 }
 
 animate()
-
-window.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "w":
-            if (player.velocity.y === 0) {
-                player.velocity.y = -20
-            }
-            break
-        case "a":
-            player.velocity.x = -4
-            break
-
-        case "d":
-            player.velocity.x = 4
-            break
-    }
-})
-
-window.addEventListener("keyup", (event) => {
-    switch (event.key) {
-
-        case "a":
-            player.velocity.x = 0
-            break
-
-        case "d":
-            player.velocity.x = 0
-            break
-    }
-})
